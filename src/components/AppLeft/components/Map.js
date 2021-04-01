6912bee2058ea1aa93d2b7b486b47b45db579b77
Andres Auais <andresauais@gmail.com>
2021-04-01 17:52:46 +0100

    map started

--- src/components/AppLeft/components/Map.js
+++ src/components/AppLeft/components/Map.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Map as LeafletMap, TileLayer } from 'react-leaflet'
 
 function Map() {
   return (
     <div className="map">
-      <h1>Map</h1>
+      <Map>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </Map>
     </div>
   )
 }
