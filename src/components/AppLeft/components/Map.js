2c2ac1920b42ecffe2301c1ee1e6fd4bab085575
Andres Auais <andresauais@gmail.com>
2021-04-01 18:16:07 +0100

    map more

--- src/components/AppLeft/components/Map.js
+++ src/components/AppLeft/components/Map.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import {Map as LeafletMap, TileLayer } from 'react-leaflet'
+import React from "react";
+import { Map as LeafletMap, MapContainer, TileLayer } from "react-leaflet";
 
-function Map() {
+function Map({ countries, casesType, center, zoom }) {
   return (
     <div className="map">
-      <Map>
+      <MapContainer center={center} zoom={zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-      </Map>
+      </MapContainer>
     </div>
-  )
+  );
 }
 
 export default Map;
